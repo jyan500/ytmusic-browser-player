15713921787f27727b80b87451205905f981f1fa
jyan500 <jyan500@gmail.com>
2025-04-10 17:10:23 -0700

    removed some un-necessary code

--- server/app.py
+++ server/app.py
@@ -11,22 +11,5 @@ CORS(app)
 Router(app)
 ErrorHandler(app)
 
-
-# @app.route("/")
-# def hello():
-#     return "Hello, Flask!"
-
-# @app.route("/authenticate", methods=["POST"])
-# def authenticate():
-#     headers = request.json["headers"]
-#     try:
-#         ytmusic = ytmusicapi.YTMusic(headers) 
-#         return jsonify(ytmusic.get_account_info()), 200
-#     except ytmusicapi.exceptions.YTMusicUserError:
-#         return jsonify({
-#             errors: ["Oops! Invalid Credentials. Make sure you're logged into music.youtube.com"]
-#         }), 401
-
-
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
