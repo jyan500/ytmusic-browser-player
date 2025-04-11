207fead4d05c433aacc85d8d71040c55d1dbe112
jyan500 <jyan500@gmail.com>
2025-04-10 17:08:35 -0700

    deleted views

--- server/base/views.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify, make_response
-from flask.views import View
-
-
-class BaseView(View):
-    @staticmethod
-    def get_response(data, status_code=200):
-        return make_response(jsonify(data), status_code)
